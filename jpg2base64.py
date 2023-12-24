04d6a9e9981282b0761fe60550631e027155e76d
hxbdy <hxbdy541@gmail.com>
2023-12-24 23:30:12 +0900

    jpgからbase64をcsv保存

--- jpg2base64.py
+++ jpg2base64.py
@@ -1,14 +1,15 @@
 import base64
 import glob
 
-files = glob.glob("./dst/*.jpg")
-
-files = files[:2]
-
+# base64エンコード結果をリストに用意する
+files = glob.glob("./past_exam_question/2023r05a_ap/question/image/image_question/*.jpg")
+b64_ary = []
 for img in files:
     with open(img,"rb") as imagefile:
-        b64_string = base64.b64encode(imagefile.read())
-        print(b64_string)
-        print()
+        b64_ary.append(base64.b64encode(imagefile.read()).decode("ascii"))
 
-
+# csv書き込み
+csv_path = "./past_exam_question/2023r05a_ap/question/csv/base64_question.csv"
+row = ",\n".join(b64_ary)
+with open(csv_path, 'w') as file:
+    file.writelines(row)
