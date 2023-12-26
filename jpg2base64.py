537aee5ff1b17ca08ec68e0e562ee5361e2398b2
hxbdy <hxbdy541@gmail.com>
2023-12-26 21:19:15 +0900

    csv形式やめる

--- jpg2base64.py
+++ jpg2base64.py
@@ -10,6 +10,6 @@ for img in files:
 
 # csv書き込み
 csv_path = "./past_exam_question/2023r05a_ap/question/csv/base64_question.csv"
-row = ",\n".join(b64_ary)
+row = "\n".join(b64_ary)
 with open(csv_path, 'w') as file:
     file.writelines(row)
